18821ba0d05792206f5cdf14aee5ae45bdbd02d5
João Paulo Cercal <jpcercal@email.com>
2016-05-18 19:16:21 -0300

    Added an abstract class that will be used in the next steps when I need create new controllers.

---
+++ src/Controller/AbstractController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Cekurte\TwitterLike\Controller;
+
+use Silex\Application;
+
+/**
+ * AbstractController
+ */
+abstract class AbstractController
+{
+    /**
+     * @var Application
+     */
+    protected $app;
+
+    /**
+     * @param Application $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * @return Silex\Application
+     */
+    public function getApp()
+    {
+        return $this->app;
+    }
+
+    /**
+     * Render a view using the Twig Template Engine
+     *
+     * @param  string $view
+     * @param  array  $params
+     *
+     * @return string
+     */
+    public function render($view, array $params = [])
+    {
+        $app = $this->getApp();
+
+        if (!isset($app['twig'])) {
+            throw new \RuntimeException('The TwigServiceProvider is not registered in this application');
+        }
+
+        $namespace = explode('\\', get_class($this));
+
+        return $app['twig']->render(sprintf('Resources/views/%s', $view), $params);
+    }
+}
